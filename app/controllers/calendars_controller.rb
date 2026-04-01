0d1373bc13998f9632fd34148261ad8faf191bd8
aka370113-afk <aka370113@gmail.com>
2026-04-01 15:16:04 +0900

    permitキー名修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :text)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
