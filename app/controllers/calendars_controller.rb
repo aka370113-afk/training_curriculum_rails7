43bacd61d00d2a6e3ef71a232f687859e633d694
aka370113-afk <aka370113@gmail.com>
2026-04-01 20:10:13 +0900

    ハッシュをシンボルへ修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -40,7 +40,7 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
        day_wday = wdays[wday_num]
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => day_wday}
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: day_wday}
       @week_days.push(days)
     end
 
